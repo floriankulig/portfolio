668f03764320d16e60c3745eb4115a2af5f3a0e9
Florian Kulig <florian.kulig@web.de>
2020-10-13 18:34:29 +0200

    started with responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./scss/App.scss";
 import { Header } from "./components/layout/Header";
 import { Content } from "./components/layout/Content";
 import { Footer } from "./components/layout/Footer";
 
 export const App = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
-      <Header />
-      <Content />
-      <Footer />
+      <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      {/* <Content />
+      <Footer /> */}
     </>
   );
 };
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 
-export const Header = () => {
+export const Header = ({ menuOpen, setMenuOpen }) => {
+
     return (
-        <nav>
-            <div className="logo">Logo</div>
-            <ul className="nav-links">
-                <li className="nav-link"></li>
-                <li className="nav-link"></li>
-                <li className="nav-link"></li>
-            </ul>
-        </nav>
+        <header>
+            <div className="container">
+                <nav>
+                    <h2 className="logo">Florian Kulig</h2>
+                    <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+                        <li className="nav-link"></li>
+                        <li className="nav-link"></li>
+                        <li className="nav-link"></li>
+                    </ul>
+                    <div
+                        className={menuOpen ? "burger open" : "burger"}
+                        onClick={() => setMenuOpen(!menuOpen)}
+                        onKeyDown={() => setMenuOpen(!menuOpen)}
+                    >
+                        <div className="burger-line"></div>
+                        <div className="burger-line"></div>
+                        <div className="burger-line"></div>
+                    </div>
+                </nav>
+            </div>
+        </header>
     )
 }
