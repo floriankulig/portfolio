0fe2632611ed2427d6fbb44cf9bad1938d1daa55
Florian Kulig <florian.kulig@web.de>
2020-10-13 21:42:49 +0200

    added Button for resume

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../Button";
+
+const navLinks = [
+    { name: "Über Mich", url: "/#about" },
+    { name: "Projekte", url: "/#projects" },
+    { name: "Kontakt", url: "/#contact" },
+    { name: "Lebenslauf", url: "/resume.pdf" }
+]
 
 export const Header = ({ menuOpen, setMenuOpen }) => {
-
     return (
         <header>
             <div className="container">
                 <nav>
                     <h2 className="logo">Florian Kulig</h2>
                     <ul className={menuOpen ? "nav-links active" : "nav-links"}>
-                        <li className="nav-link"></li>
-                        <li className="nav-link"></li>
-                        <li className="nav-link"></li>
+                        {navLinks &&
+                            navLinks.map(({ url, name }, i) =>
+                                name === "Lebenslauf" ? (
+                                    <li className="nav-link">
+                                        <Button
+                                            basic={false}
+                                            style={{ animationDelay: `${(navLinks.length + 1) * 50}ms` }}
+                                        >
+                                            {name}
+                                        </Button>
+                                    </li>
+
+                                ) : (
+                                        <li
+                                            className="nav-link"
+                                            style={{
+                                                animationDelay: `${i * 50}ms`
+                                            }}>
+                                            0{i + 1}. <a href={url}>{name}</a>
+                                        </li>
+                                    )
+                            )}
+                        <li></li>
                     </ul>
                     <div
                         className={menuOpen ? "burger open" : "burger"}
@@ -22,7 +49,7 @@ export const Header = ({ menuOpen, setMenuOpen }) => {
                         <div className="burger-line"></div>
                     </div>
                 </nav>
-            </div>
-        </header>
-    )
-}
+            </div >
+        </header >
+    );
+};
