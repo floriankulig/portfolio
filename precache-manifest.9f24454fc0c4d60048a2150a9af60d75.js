381df143b92ae3a3559e48686cd0f481af0ba7d7
Florian Kulig <florian.kulig@web.de>
2020-10-14 19:58:19 +0200

    Updates

---
+++ precache-manifest.9f24454fc0c4d60048a2150a9af60d75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36358b7a7ae05f17eacc693b964e124f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "517f12ed6b5d8621099c",
+    "url": "/portfolio/static/css/main.2e3cc333.chunk.css"
+  },
+  {
+    "revision": "6af3d869ce6ebc6bc825",
+    "url": "/portfolio/static/js/2.67ec76ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.67ec76ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517f12ed6b5d8621099c",
+    "url": "/portfolio/static/js/main.3cd27968.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
