64a4bb4f23e91aace146541c78c7a59e33cce780
Florian Kulig <florian.kulig@web.de>
2020-10-15 21:34:20 +0200

    added hero section

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Hero } from "../sections/Hero";
 
 export const Content = () => {
-  return <div>Content</div>;
+  return (
+    <div className="container content">
+      <main className="main-col">
+        <section className="hero"><Hero /></section>
+      </main>
+    </div>
+  );
 };
--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
     <h1 className="fade-up">Florian Kulig.</h1>
     <h1 className="short-desc fade-up">Ich entwickle Projekte für das Web.</h1>
     <p className="desc fade-up">
-      Ich bin ein Schüler aus Treuchtlingen, Bayern, der Spaß daran hat ausgezeichnete Websites, Apps und Alles, was dazwischen liegt, zu (designen und) entwickeln.
+      Ich bin ein Schüler aus Treuchtlingen, Bayern, der Spaß daran hat designbetonte Websites, Apps und Alles, was dazu gehört, zu (designen und) entwickeln.
     </p>
     <a href="mailto:florian.kulig@web.de">
       <Button className="cta fade-up" basic={true}>
