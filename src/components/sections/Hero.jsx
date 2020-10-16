8a0f069b0cf9d75f31f0d92b276c9ac582c584e3
Florian Kulig <florian.kulig@web.de>
2020-10-16 20:33:03 +0200

    Changed text

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import { Button } from "../Button";
 
 export const Hero = () => {
-  return <>
-    <h3 className="introduction fade-up">Hallo, ich bin</h3>
-    <h1 className="fade-up">Florian Kulig.</h1>
-    <h1 className="short-desc fade-up">Ich entwickle Projekte für das Web.</h1>
-    <p className="desc fade-up">
-      Ich bin ein Schüler aus Treuchtlingen, Bayern, der Spaß daran hat designbetonte Websites, Apps und Alles, was dazu gehört, zu (designen und) entwickeln.
-    </p>
-    <a href="mailto:florian.kulig@web.de">
-      <Button className="cta fade-up" basic={true}>
-        Lass uns loslegen
-      </Button>
-    </a>
-  </>;
+  return (
+    <>
+      <h3 className="introduction fade-up">Hallo, ich bin</h3>
+      <h1 className="fade-up">Florian Kulig.</h1>
+      <h1 className="short-desc fade-up">Ich entwickle für das Web.</h1>
+      <p className="desc fade-up">
+        Ich bin ein Schüler aus Treuchtlingen, Bayern, der Spaß daran hat
+        designbetonte Websites, Apps und Alles, was dazu gehört, zu (designen
+        und) entwickeln.
+      </p>
+      <a href="mailto:florian.kulig@web.de">
+        <Button className="cta fade-up" basic={true}>
+          Lass uns loslegen
+        </Button>
+      </a>
+    </>
+  );
 };
