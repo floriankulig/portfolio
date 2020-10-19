b9407efad4928b1f76e287d482397252a3efca38
Florian Kulig <florian.kulig@web.de>
2020-10-19 18:48:12 +0200

    Added About section

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { About } from "../sections/About";
 import { Hero } from "../sections/Hero";
 
 export const Content = () => {
   return (
     <div className="container content">
       <main className="main-col">
-        <section className="hero"><Hero /></section>
+        <section className="hero">
+          <Hero />
+        </section>
+        <section className="about section">
+          <h2 className="section__title">
+            <span className="section__title-idx">01. </span>Über mich
+          </h2>
+          <About />
+        </section>
       </main>
     </div>
   );
