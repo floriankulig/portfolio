b1a1dca9be85b632fc15a180d7c81f8bfd579ad0
Florian Kulig <florian.kulig@web.de>
2020-10-19 19:45:08 +0200

    added basic description for about section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export const About = () => {
-  return <div></div>;
+  return (
+    <>
+      <p>Hallo, ich bin Flo, Schüler und Hobbyentwickler aus Treuchtlingen, Bayern.</p>
+      <p>Mit Hilfe von modernen Web-Technologien gestalte ich Webseiten, Apps und alles, was dazu gehört. Das Ziel ist meistens gleich: Den Nutzern eine detailgetreue und performante Erfahrung bieten.</p>
+      <p>Seit meinem Praktikum bei der Firma Alfmeier setze ich mich mit Software-Engineering auseinander und habe einige Projekte in die Tat umgesetzt. </p>
+    </>
+  );
 };
