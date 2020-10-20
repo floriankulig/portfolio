0aa9ece1444b902e7e79ca332bb62f10de15e39e
Florian Kulig <florian.kulig@web.de>
2020-10-20 20:21:52 +0200

    added coder svg to markup

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { ReactComponent as Coder } from "../../images/coder.svg"
 
 export const About = () => {
   return (
     <>
-      <p>Hallo, ich bin Flo, Schüler und Hobbyentwickler aus Treuchtlingen, Bayern.</p>
-      <p>Mit Hilfe von modernen Web-Technologien gestalte ich Webseiten, Apps und alles, was dazu gehört. Das Ziel ist meistens gleich: Den Nutzern eine detailgetreue und performante Erfahrung bieten.</p>
-      <p>Seit meinem Praktikum bei der Firma Alfmeier setze ich mich mit Software-Engineering auseinander und habe einige Projekte in die Tat umgesetzt. </p>
+      <div className="section__inner-desc">
+        <p>Hallo, ich bin Flo, Schüler und Hobbyentwickler aus Treuchtlingen, Bayern.</p>
+        <p>Mit Hilfe von modernen Web-Technologien gestalte ich Webseiten, Apps und alles, was dazu gehört. Das Ziel ist meistens gleich: Den Nutzern eine detailgetreue und performante Erfahrung bieten.</p>
+        <p>Seit meinem Praktikum bei der Firma Alfmeier setze ich mich mit Software-Engineering auseinander und habe einige Projekte in die Tat umgesetzt. </p>
+      </div>
+      <div className="section__inner-svg">
+        <Coder />
+      </div>
     </>
   );
 };
