a4bb086c87b1e825a8cf99a4f2cb210e8a4f0f3d
Florian Kulig <florian.kulig@web.de>
2020-10-21 21:04:06 +0200

    added skills to section content

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactComponent as Coder } from "../../images/coder.svg"
+import { Skills } from "../Skills";
 
 export const About = () => {
   return (
@@ -8,6 +9,8 @@ export const About = () => {
         <p>Hallo, ich bin Flo, Schüler und Hobbyentwickler aus Treuchtlingen, Bayern.</p>
         <p>Mit Hilfe von modernen Web-Technologien gestalte ich Webseiten, Apps und alles, was dazu gehört. Das Ziel ist meistens gleich: Den Nutzern eine detailgetreue und performante Erfahrung bieten.</p>
         <p>Seit meinem Praktikum bei der Firma Alfmeier setze ich mich mit Software-Engineering auseinander und habe einige Projekte in die Tat umgesetzt. </p>
+        <p>Hier sind ein paar Technologien, die ich in letzter Zeit benutzt habe:</p>
+        <Skills />
       </div>
       <div className="section__inner-svg">
         <Coder />
