95d92f4ebcd2642fe9e6e1486ac49dd9f6cbab52
Florian Kulig <florian.kulig@web.de>
2020-10-21 21:03:06 +0200

    added skills component

---
+++ src/components/Skills.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Skills = () => {
+    return (
+        <ul className="skills">
+            <li>JavaScript (ES6+)</li>
+            <li>React</li>
+            <li>HTML & (S)CSS</li>
+            <li>Firebase</li>
+            <li>Python</li>
+            <li>Figma</li>
+        </ul>
+    )
+}
