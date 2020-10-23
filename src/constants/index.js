db0e1cf8765a0f52cfb02ff2468fa8027046a54f
Florian Kulig <florian.kulig@web.de>
2020-10-23 22:14:22 +0200

    moved navLinks to constants file

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,8 @@
 export const overlayDuration = 5000; //in ms
+
+export const navLinks = [
+  { name: "Über Mich", url: "/#about" },
+  { name: "Projekte", url: "/#projects" },
+  { name: "Kontakt", url: "/#contact" },
+  { name: "Lebenslauf", url: "/resume.pdf" },
+];
