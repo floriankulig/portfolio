94c966c1dea663e93dcccb950ffca7af8f938ed3
Florian Kulig <florian.kulig@web.de>
2020-10-23 22:14:37 +0200

    added copyToClipboard function

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,8 +1,3 @@
-export const baseUrl = "portfolio";
-
-export const navLinks = [
-  { name: "Über Mich", url: "/#about" },
-  { name: "Projekte", url: "/#projects" },
-  { name: "Kontakt", url: "/#contact" },
-  { name: "Lebenslauf", url: "/resume.pdf" },
-];
+export const copyToClipboard = (text) => {
+  navigator.clipboard.writeText(text);
+};
