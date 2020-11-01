c09a199e1928b092b71e2731ce3f84df9ed006fd
Florian Kulig <florian.kulig@web.de>
2020-11-01 20:53:08 +0100

    removed unneccesary var

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,41 +1,42 @@
 import React, { useState } from "react";
 import { copyToClipboard } from "../../helpers";
 import { Button } from "../Button";
-import { overlayDuration, email } from "../../constants"
+import { overlayDuration, email } from "../../constants";
 
 export const Contact = () => {
-    const [snackbarOpen, setSnackbarOpen] = useState(false);
-    const handleEmailClick = () => {
-        copyToClipboard(email);
-        setSnackbarOpen(true);
-        //+ 500: basic slideIn/Out animation duration
-        setTimeout(() => setSnackbarOpen(false), overlayDuration)
-        clearTimeout()
-    };
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const handleEmailClick = () => {
+    copyToClipboard(email);
+    setSnackbarOpen(true);
+    //+ 500: basic slideIn/Out animation duration
+    setTimeout(() => setSnackbarOpen(false), 5000);
+    clearTimeout();
+  };
 
-    return (
-        <>
-            <p>
-                Überzeugt vom Portfolio? Fragen zu mir? Egal, was es ist, mein Postfach
-                steht Ihnen offen. Schreiben Sie eine Mail an{" "}
-                <span
-                    className="contact__mail"
-                    onClick={() => handleEmailClick()}
-                    onKeyDown={() => handleEmailClick()}
-                >
-                    {email}
-                </span>{" "}
-                oder klicken Sie unten.
-             </p>
+  return (
+    <>
+      <p>
+        Überzeugt vom Portfolio? Fragen zu mir? Egal, was es ist, mein Postfach
+        steht Ihnen offen. Schreiben Sie eine Mail an{" "}
+        <span
+          className="contact__mail"
+          onClick={() => handleEmailClick()}
+          onKeyDown={() => handleEmailClick()}
+        >
+          {email}
+        </span>{" "}
+        oder klicken Sie unten.
+      </p>
 
-            <a href={`mailto:${email}`}>
-                <Button basic={true}>Hallo sagen</Button>
-            </a>
+      <a href={`mailto:${email}`}>
+        <Button basic={true}>Hallo sagen</Button>
+      </a>
 
-
-            <div className={snackbarOpen ? "snackbar open" : "snackbar"}>
-                <p className="snackbar__message">Email in die Zwischenablage kopiert!</p>
-            </div>
-        </>
-    );
+      <div className={snackbarOpen ? "snackbar open" : "snackbar"}>
+        <p className="snackbar__message">
+          Email in die Zwischenablage kopiert!
+        </p>
+      </div>
+    </>
+  );
 };
