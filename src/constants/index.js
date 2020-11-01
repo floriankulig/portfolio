e51b4a4b63fd9111e22ec62829d96845001653ff
Florian Kulig <florian.kulig@web.de>
2020-11-01 20:51:19 +0100

    added file path for project images

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,29 @@
-export const overlayDuration = 5000; //in ms
-
 export const navLinks = [
   { name: "Über Mich", url: "/#about" },
   { name: "Projekte", url: "/#projects" },
   { name: "Kontakt", url: "/#contact" },
   { name: "Lebenslauf", url: "/resume.pdf" },
+];
+
+export const projects = [
+  {
+    title: "Todoist Clone",
+    desc:
+      "Ein Klon der Web-App von Todoist. Der Nutzer kann eigene Projekte erstellen, die wiederrum eigene ToDos haben. Diese werden sofort in die Datenbank eingetragen und auf allen eingeloggten Geräten synchronisiert.",
+    techStack: ["React", "Firebase", "SCSS"],
+    github: "https://github.com/floriankulig/Todoist-Clone/",
+    url: "https://floriankulig.github.io/Todoist-Clone/",
+    img: "todoist.png",
+  },
+  {
+    title: "Sorting Algorithm Visualizer",
+    desc:
+      "Eine Desktop-App mit der verschiedene Sortier-Algorithmen visualisiert werden können. Die Elemente, die in der zu sortierenden Liste sind, werden als Säulen mit verschiedenen Höhen dargestellt.",
+    techStack: ["Python", "Tkinter"],
+    github: "https://github.com/floriankulig/SortingVisualizer",
+    url: "",
+    img: null,
+  },
 ];
 
 export const email = "florian.kulig@web.de";
