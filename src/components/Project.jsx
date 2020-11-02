ea522ffba3e47ca494d33b408c7a1184836a652b
Florian Kulig <florian.kulig@web.de>
2020-11-02 21:36:20 +0100

    finished project strucure

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa"
 
 export const Project = ({ project }) => {
     const { title, desc, techStack, github, url, img } = project;
     return (
-        <>
-            {/* {img && <img src={require(`../images/project-images/${img}`)} alt={title} />} */}
-            <h3 className="project__title">{title}</h3>
-            <p className="project__desc">{desc}</p>
-            {techStack && techStack.map((tool, i) => (
-                <span key={i}>{tool}</span>
-            ))}
-        </>
+        <div className="project">
+            {img && <img src={require(`../images/project-images/${img}`)} alt={title} />}
+            <div className="project__desc">
+                <h3 className="project__desc-title">{title}</h3>
+                <p className="project__desc-info">{desc}</p>
+                {techStack && techStack.map((tool, i) => (
+                    <span key={i}>{tool}</span>
+                ))}
+
+                <ul className="project__desc-links">
+                    <a href={url} target="_blank" rel="noopener noreferrer">
+                        <FaExternalLinkAlt />
+                    </a>
+                    <a href={github} target="_blank" rel="noopener noreferrer">
+                        <FaGithub />
+                    </a>
+                </ul>
+            </div>
+        </div>
     );
 };
