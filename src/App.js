fbb8740e85edaaf61e6cb1e919313baa31b409f5
Florian Kulig <florian.kulig@web.de>
2020-11-05 23:13:10 +0100

    scrollOut Config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ScrollOut from "scroll-out";
 import "./scss/App.scss";
 import { Header } from "./components/layout/Header";
 import { Content } from "./components/layout/Content";
 import { Footer } from "./components/layout/Footer";
 
 export const App = () => {
+  useEffect(() => {
+    ScrollOut({
+      once: true,
+      threshold: 0.5,
+    });
+  }, []);
+
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
