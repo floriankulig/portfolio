c90018c28d9a82bf490195aa876093f048c5f0c6
Florian Kulig <florian.kulig@web.de>
2020-11-10 18:15:03 +0100

    added component for loading screen

---
+++ src/components/layout/Loading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ReactComponent as Logo } from "../../images/logo.svg";
+
+export const Loading = () => {
+  return (
+    <div className="loading-view">
+      <Logo />
+    </div>
+  );
+};
