631e4518e9d40f45f593f45dbef02b4287b19a45
Florian Kulig <florian.kulig@web.de>
2020-11-10 18:15:38 +0100

    added logic for displaying loading screen

--- src/App.js
+++ src/App.js
@@ -4,22 +4,36 @@ import "./scss/App.scss";
 import { Header } from "./components/layout/Header";
 import { Content } from "./components/layout/Content";
 import { Footer } from "./components/layout/Footer";
+import { Loading } from "./components/layout/Loading";
 
 export const App = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    ScrollOut({
-      once: true,
-      threshold: 0.4,
-    });
+    const timeout = setTimeout(() => {
+      setLoading(false);
+      document.body.style.overflow = null;
+    }, 5000);
+    return () => clearTimeout(timeout);
   }, []);
 
-  const [menuOpen, setMenuOpen] = useState(false);
+  useEffect(() => {
+    if (!loading) {
+      ScrollOut({
+        once: true,
+        threshold: 0.4,
+      });
+    }
+  }, [loading]);
 
-  return (
+  return loading ? (
+    <Loading loading />
+  ) : (
     <>
       <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <Content />
-      {/*<Footer /> */}
+      <Footer />
     </>
   );
 };
