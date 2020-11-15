af09fb9b6a0906b862b507ee49b189625ddb5a14
Florian Kulig <florian.kulig@web.de>
2020-11-15 13:24:40 +0100

    handling transition logic in component

--- src/components/Snackbar.jsx
+++ src/components/Snackbar.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const Snackbar = ({ children }) => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const timeout1 = setTimeout(() => setOpen(true), 1);
+    const timeout2 = setTimeout(() => setOpen(false), 4600);
+    return () => {
+      clearTimeout(timeout1);
+      clearTimeout(timeout2);
+    };
+  }, []);
+
   return (
-    <div className="snackbar">
+    <div className={open ? "snackbar open" : "snackbar"}>
       <p className="snackbar__message">{children}</p>
     </div>
   );
