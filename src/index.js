837001acddb44ce834490894598e4e5f9b33e918
Florian Kulig <florian.kulig@web.de>
2020-11-20 22:12:39 +0100

    set up project for multiple languages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+
+import common_de from "./languages/common_de.json";
+import common_en from "./languages/common_en.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    en: {
+      common: common_en,
+    },
+    de: {
+      common: common_de,
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
