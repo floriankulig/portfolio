806840709b6f12819376a6609634e13c6bb9676d
Florian Kulig <florian.kulig@web.de>
2020-12-05 19:01:29 +0100

    added language changing tests

--- src/__tests__/ChangeLanguage.spec.js
+++ src/__tests__/ChangeLanguage.spec.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { render, cleanup, fireEvent } from "@testing-library/react";
 import { ChangeLanguage } from "../components/ChangeLanguage";
+import i18next from "i18next";
+
+jest.mock("i18next", () => ({
+  changeLanguage: jest.fn(),
+  language: "en",
+}));
 
 beforeEach(cleanup);
 
@@ -26,5 +32,19 @@ describe("<ChangeLanguage/>", () => {
     expect(
       queryByTestId("lang-changer").classList.contains("open")
     ).toBeTruthy();
+  });
+
+  it("changes the language to english using onClick", () => {
+    const { queryByTestId } = render(<ChangeLanguage />);
+    fireEvent.click(queryByTestId("lang-changer-open"));
+    fireEvent.click(queryByTestId("lang-option__english"));
+    expect(i18next.language === "en").toBeTruthy();
+  });
+
+  it("changes the language to english using onKeyDown", () => {
+    const { queryByTestId } = render(<ChangeLanguage />);
+    fireEvent.keyDown(queryByTestId("lang-changer-open"));
+    fireEvent.keyDown(queryByTestId("lang-option__english"));
+    expect(i18next.language === "en").toBeTruthy();
   });
 });
