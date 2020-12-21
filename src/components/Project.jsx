78707ea7d30f82643db31c9d3ecaf549e32b5af8
Florian Kulig <florian.kulig@web.de>
2020-12-21 18:20:24 +0100

    setting inner html to json data

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,10 +7,10 @@ export const Project = ({ project }) => {
         <div className="project">
             {img && <img src={require(`../images/project-images/${img}`)} alt={title} />}
             <div className="project__desc">
-                <h3 className="project__desc-title">{title}</h3>
+                <h3 className="project__desc-title" dangerouslySetInnerHTML={{ __html: title }} />
                 <p className="project__desc-info">{desc}</p>
                 {techStack && techStack.map((tool, i) => (
-                    <span key={i}>{tool}</span>
+                    <span key={i} dangerouslySetInnerHTML={{ __html: tool }} />
                 ))}
 
                 <ul className="project__desc-links">
@@ -22,6 +22,6 @@ export const Project = ({ project }) => {
                     </a>
                 </ul>
             </div>
-        </div>
+        </div >
     );
 };
