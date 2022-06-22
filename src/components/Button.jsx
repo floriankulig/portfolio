be59d4816861f7df6e2e0a16ef32cba691c24a1a
Florian Kulig <florian.kulig@web.de>
2022-06-22 20:33:30 +0200

    passing disabled prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-export const Button = ({ children, basic = false }) => {
+export const Button = ({ children, basic = false, disabled }) => {
   return (
-    <button className={basic ? "btn basic" : "btn"} tabIndex={0}>
+    <button
+      className={basic ? "btn basic" : "btn"}
+      disabled={disabled}
+      tabIndex={0}
+    >
       {children}
     </button>
   );
